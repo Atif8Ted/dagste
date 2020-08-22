8687174e84189792a7595e96f88fd8bf1cd9d727
Atif Imam <mohd.atifimam@hotmail.com>
2020-08-22 17:12:36 +0530

    updated

---
+++ hello_dagster.py
@@ -0,0 +1,17 @@
+from dagster import execute_pipeline , pipeline,solid
+
+@solid
+def get_name(_):
+    return 'dagster'
+
+@solid
+def hello(context,name:str):
+    context.log.info("Hello,{0}!".format(name))
+
+
+@pipeline
+def hello_pipeline():
+    hello(get_name())
+
+if __name__ == "__main__":
+    execute_pipeline(hello_pipeline) 
